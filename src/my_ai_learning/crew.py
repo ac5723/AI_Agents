7ec67c7100326f9c69bae656b7d2d4cfcc4127f3
ac5723 <arpit.chourdiya@yahoo.com>
2026-04-01 01:27:06 +0900

    Updated code. Added new agents and task

--- src/my_ai_learning/crew.py
+++ src/my_ai_learning/crew.py
@@ -1,63 +1,125 @@
-from crewai import Agent, Crew, Process, Task
+import os
+from crewai import Agent, Crew, Process, Task, LLM
 from crewai.project import CrewBase, agent, crew, task
-from crewai.agents.agent_builder.base_agent import BaseAgent
-# If you want to run a snippet of code before or after the crew starts,
-# you can use the @before_kickoff and @after_kickoff decorators
-# https://docs.crewai.com/concepts/crews#example-crew-class-with-decorators
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = LLM(
+    model="openai/gpt-oss-120b",
+    api_key=os.getenv("OPENROUTER_API_KEY"),
+    base_url="https://openrouter.ai/api/v1",
+)
 
 @CrewBase
 class MyAiLearning():
-    """MyAiLearning crew"""
 
-    agents: list[BaseAgent]
-    tasks: list[Task]
+    agents_config = 'config/agents.yaml'
+    tasks_config = 'config/tasks.yaml'
 
-    # Learn more about YAML configuration files here:
-    # Agents: https://docs.crewai.com/concepts/agents#yaml-configuration-recommended
-    # Tasks: https://docs.crewai.com/concepts/tasks#yaml-configuration-recommended
-    
-    # If you would like to add tools to your agents, you can learn more about it here:
-    # https://docs.crewai.com/concepts/agents#agent-tools
+    # --- Agents ---
     @agent
     def researcher(self) -> Agent:
         return Agent(
-            config=self.agents_config['researcher'], # type: ignore[index]
+            config=self.agents_config['researcher'],
+            llm=llm,
             verbose=True
         )
 
     @agent
-    def reporting_analyst(self) -> Agent:
+    def summarizer(self) -> Agent:
         return Agent(
-            config=self.agents_config['reporting_analyst'], # type: ignore[index]
+            config=self.agents_config['summarizer'],
+            llm=llm,
             verbose=True
         )
 
-    # To learn more about structured task outputs,
-    # task dependencies, and task callbacks, check out the documentation:
-    # https://docs.crewai.com/concepts/tasks#overview-of-a-task
+    # --- Tasks ---
     @task
     def research_task(self) -> Task:
         return Task(
-            config=self.tasks_config['research_task'], # type: ignore[index]
+            config=self.tasks_config['research_task'],
+            agent=self.researcher()
         )
 
     @task
-    def reporting_task(self) -> Task:
+    def summarize_task(self) -> Task:
         return Task(
-            config=self.tasks_config['reporting_task'], # type: ignore[index]
-            output_file='report.md'
+            config=self.tasks_config['summarize_task'],
+            agent=self.summarizer(),
+            context=[self.research_task()]  # uses research output
         )
 
+    # --- Crew ---
     @crew
     def crew(self) -> Crew:
-        """Creates the MyAiLearning crew"""
-        # To learn how to add knowledge sources to your crew, check out the documentation:
-        # https://docs.crewai.com/concepts/knowledge#what-is-knowledge
-
         return Crew(
-            agents=self.agents, # Automatically created by the @agent decorator
-            tasks=self.tasks, # Automatically created by the @task decorator
-            process=Process.sequential,
-            verbose=True,
-            # process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
+            agents=self.agents,
+            tasks=self.tasks,
+            process=Process.sequential,  # tasks run one after another
+            verbose=True
         )
+
+# from crewai import Agent, Crew, Process, Task
+# from crewai.project import CrewBase, agent, crew, task
+# from crewai.agents.agent_builder.base_agent import BaseAgent
+# # If you want to run a snippet of code before or after the crew starts,
+# # you can use the @before_kickoff and @after_kickoff decorators
+# # https://docs.crewai.com/concepts/crews#example-crew-class-with-decorators
+#
+# @CrewBase
+# class MyAiLearning():
+#     """MyAiLearning crew"""
+#
+#     agents: list[BaseAgent]
+#     tasks: list[Task]
+#
+#     # Learn more about YAML configuration files here:
+#     # Agents: https://docs.crewai.com/concepts/agents#yaml-configuration-recommended
+#     # Tasks: https://docs.crewai.com/concepts/tasks#yaml-configuration-recommended
+#
+#     # If you would like to add tools to your agents, you can learn more about it here:
+#     # https://docs.crewai.com/concepts/agents#agent-tools
+#     @agent
+#     def researcher(self) -> Agent:
+#         return Agent(
+#             config=self.agents_config['researcher'], # type: ignore[index]
+#             verbose=True
+#         )
+#
+#     @agent
+#     def reporting_analyst(self) -> Agent:
+#         return Agent(
+#             config=self.agents_config['reporting_analyst'], # type: ignore[index]
+#             verbose=True
+#         )
+#
+#     # To learn more about structured task outputs,
+#     # task dependencies, and task callbacks, check out the documentation:
+#     # https://docs.crewai.com/concepts/tasks#overview-of-a-task
+#     @task
+#     def research_task(self) -> Task:
+#         return Task(
+#             config=self.tasks_config['research_task'], # type: ignore[index]
+#         )
+#
+#     @task
+#     def reporting_task(self) -> Task:
+#         return Task(
+#             config=self.tasks_config['reporting_task'], # type: ignore[index]
+#             output_file='report.md'
+#         )
+#
+#     @crew
+#     def crew(self) -> Crew:
+#         """Creates the MyAiLearning crew"""
+#         # To learn how to add knowledge sources to your crew, check out the documentation:
+#         # https://docs.crewai.com/concepts/knowledge#what-is-knowledge
+#
+#         return Crew(
+#             agents=self.agents, # Automatically created by the @agent decorator
+#             tasks=self.tasks, # Automatically created by the @task decorator
+#             process=Process.sequential,
+#             verbose=True,
+#             # process=Process.hierarchical, # In case you wanna use that instead https://docs.crewai.com/how-to/Hierarchical/
+#         )
--- src/my_ai_learning/main.py
+++ src/my_ai_learning/main.py
@@ -1,3 +1,15 @@
+# from my_ai_learning.crew import MyAiLearning
+#
+# def run():
+#     inputs = {
+#         "topic": "Artificial Intelligence"  # 👈 change this to anything!
+#     }
+#     MyAiLearning().crew().kickoff(inputs=inputs)
+#
+# # if run == "__main__":
+# #     run()
+# run()
+
 import os
 
 from crewai import Crew
